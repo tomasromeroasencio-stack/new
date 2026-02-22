29da91437b5064a88a8f22dd8e4a7c9f8161c76f
Tomas Romero <tomasromeroasencio@gmail.com>
2026-02-21 22:05:41 -0400

    suma elementos de la lista

--- index.py
+++ index.py
@@ -1,2 +1,8 @@
 lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(f"la longitud de la lista es: {len(lista)}")
+
+suma = 0
+for i in lista :
+    suma += i
+
+print(f"\nla suma de los valores de la lista es : {suma}")    
